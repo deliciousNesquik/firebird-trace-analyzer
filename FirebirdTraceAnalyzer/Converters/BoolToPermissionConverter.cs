454d2ecc87459136eee56806363619971890f57c
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-17 16:37:21 +0500

    refactor(converters): rewrite docs and update logic
    update doc-string -> translate to eng and add remarks
    clean code "Guard Clauses"

--- FirebirdTraceAnalyzer/Converters/BoolToPermissionConverter.cs
+++ FirebirdTraceAnalyzer/Converters/BoolToPermissionConverter.cs
@@ -3,22 +3,20 @@ using Avalonia.Data.Converters;
 
 namespace FirebirdTraceAnalyzer.Converters;
 
-/// <summary>
-/// Конвертирует bool прав доступа в символ (r/w/x или -)
-/// </summary>
+/// <summary>Converts a boolean value into the passed access permission character (r/w/x или -).</summary>
+/// <returns>The permission character if the boolean value is true, otherwise "-".</returns>
+/// <remarks>Character use for not having permission is "-" hardcoded if you need a different one, modify the return values accordingly</remarks>
 public class BoolToPermissionConverter : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is bool hasPermission && parameter is string permissionChar)
-        {
-            return hasPermission ? permissionChar : "-";
-        }
+        if (value is not bool hasPermission || parameter is not string permissionChar)
+            return "-";
 
-        return "-";
+        return hasPermission ? permissionChar : "-";
     }
 
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
     }
