79d2474cff25e733aaeb883739c4dc746cc317b4
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-17 16:11:30 +0500

    refactor(converters): rewrite docs and update logic
    update doc-string -> translate to eng and add remarks
    clean code "Guard Clauses"

--- FirebirdTraceAnalyzer/Converters/BoolToPasswordCharConverter.cs
+++ FirebirdTraceAnalyzer/Converters/BoolToPasswordCharConverter.cs
@@ -3,26 +3,21 @@ using Avalonia.Data.Converters;
 
 namespace FirebirdTraceAnalyzer.Converters;
 
-/// <summary>
-/// Конвертирует bool в символ маскировки пароля
-/// true = показать пароль (0 - без маскировки)
-/// false = скрыть пароль ('•')
-/// </summary>
+/// <summary>Converts a boolean value into a string masking character.</summary>
+/// <returns>(\0 - without masking) if true and ('*' - masking character) if false</returns>
+/// <remarks>masking character hardcoded, if you need a different one, modify the return values accordingly</remarks>
 public class BoolToPasswordCharConverter : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is bool showPassword)
-        {
-            // Если showPassword = true, возвращаем '\0' (нет маскировки)
-            // Если showPassword = false, возвращаем '•' (маскировка)
-            return showPassword ? '\0' : '*';
-        }
+        if (value is not bool showPassword)
+            return '*'; // By default, mask the password
 
-        return '•'; // По умолчанию маскируем
+        // The "\0" character or attribute overrides imposed string formatting patterns
+        return showPassword ? '\0' : '*';
     }
 
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
     }
