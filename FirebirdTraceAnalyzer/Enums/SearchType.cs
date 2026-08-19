e9e1da6d3bec88164db5a840a0ebc16da0917d5f
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-19 11:15:18 +0500

    docs(enum): add XML documentation for SearchType enumeration values

--- FirebirdTraceAnalyzer/Enums/SearchType.cs
+++ FirebirdTraceAnalyzer/Enums/SearchType.cs
@@ -1,7 +1,17 @@
 ﻿namespace FirebirdTraceAnalyzer.Enums;
 
+/// <summary>
+/// Defines the types of search operations that can be performed.
+/// </summary>
 public enum SearchType
 {
+    /// <summary>
+    /// Represents a classic search type, which typically involves simple string matching.
+    /// </summary>
     Classic,
+    
+    /// <summary>
+    /// Represents a regex search type, which allows for more complex pattern matching using regular expressions.
+    /// </summary>
     Regex
 }
