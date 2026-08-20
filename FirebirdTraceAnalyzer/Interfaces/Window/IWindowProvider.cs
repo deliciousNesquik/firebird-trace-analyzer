cc69b5514869a027cc8e187f3c24810d91df791e
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-20 09:25:55 +0500

    docs(IWindowProvider): enhance XML documentation for top-level window retrieval

--- FirebirdTraceAnalyzer/Interfaces/Window/IWindowProvider.cs
+++ FirebirdTraceAnalyzer/Interfaces/Window/IWindowProvider.cs
@@ -2,7 +2,14 @@
 
 namespace FirebirdTraceAnalyzer.Interfaces.Window;
 
+/// <summary>
+/// Defines a provider for obtaining the current top-level window in an Avalonia application.
+/// </summary>
 public interface IWindowProvider
 {
+    /// <summary>
+    /// Gets the current top-level window. <see cref="TopLevel"/>
+    /// </summary>
+    /// <returns>The current top-level window or null if none is available.</returns>
     TopLevel? GetCurrent();
 }
