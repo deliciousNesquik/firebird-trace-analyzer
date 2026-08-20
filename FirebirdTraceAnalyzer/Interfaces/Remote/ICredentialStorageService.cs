677125858ec60488f3e657edab3c79be6dd9f373
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-20 08:43:38 +0500

    docs(ICredentialStorageService): enhance XML documentation for credential storage methods

--- FirebirdTraceAnalyzer/Interfaces/Remote/ICredentialStorageService.cs
+++ FirebirdTraceAnalyzer/Interfaces/Remote/ICredentialStorageService.cs
@@ -1,19 +1,41 @@
 ﻿namespace FirebirdTraceAnalyzer.Interfaces.Remote;
 
 /// <summary>
-/// Сервис для безопасного хранения учётных данных
+/// Interface for a service that handles credential storage, including saving,
+/// retrieving, and deleting passwords for given servers and usernames.
 /// </summary>
 public interface ICredentialStorageService
 {
-    /// <summary>Сохранить пароль</summary>
+    /// <summary>
+    /// Save password for a given server and username
+    /// </summary>
+    /// <param name="server">The server for which to save the password.</param>
+    /// <param name="username">The username for which to save the password.</param>
+    /// <param name="password">The password to save.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task SavePasswordAsync(string server, string username, string password);
     
-    /// <summary>Получить пароль</summary>
+    /// <summary>
+    /// Gets the password for a given server and username.
+    /// </summary>
+    /// <param name="server">The server for which to get the password.</param>
+    /// <param name="username">The username for which to get the password.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task<string?> GetPasswordAsync(string server, string username);
     
-    /// <summary>Удалить пароль</summary>
+    /// <summary>
+    /// Deletes the password for a given server and username.
+    /// </summary>
+    /// <param name="server">The server for which to delete the password.</param>
+    /// <param name="username">The username for which to delete the password.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task DeletePasswordAsync(string server, string username);
     
-    /// <summary>Проверить наличие сохранённого пароля</summary>
+    /// <summary>
+    /// Checks if a password exists for a given server and username.
+    /// </summary>
+    /// <param name="server">The server for which to check the password.</param>
+    /// <param name="username">The username for which to check the password.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task<bool> HasPasswordAsync(string server, string username);
 }
