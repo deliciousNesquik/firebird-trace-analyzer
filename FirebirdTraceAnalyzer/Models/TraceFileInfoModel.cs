e30b6de7ca5e3cde0a8079f229d47868bc989d32
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-20 13:40:38 +0500

    docs(TraceFileInfoModel): enhance XML documentation for the trace file info model

--- FirebirdTraceAnalyzer/Models/TraceFileInfoModel.cs
+++ FirebirdTraceAnalyzer/Models/TraceFileInfoModel.cs
@@ -1,8 +1,15 @@
 ﻿namespace FirebirdTraceAnalyzer.Models;
 
 /// <summary>
-/// Неизменяемая модель информации о загруженном trace-файле.
+/// Immutable information about a loaded trace file.
 /// </summary>
+/// <param name="FileName">File name.</param>
+/// <param name="FilePath">Full path to the file.</param>
+/// <param name="FileSize">File size in bytes.</param>
+/// <param name="StartTrace">Timestamp of the first event in the file.</param>
+/// <param name="EndTrace">Timestamp of the last event in the file.</param>
+/// <param name="EventCount">Number of events parsed from the file.</param>
+/// <param name="FileHash">Content hash used to identify the file.</param>
 public sealed record TraceFileInfoModel(
     string FileName,
     string FilePath,
