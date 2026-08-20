abc7a738dfcb519697ca1d3f4dd8d27c0c1f92eb
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-20 09:56:08 +0500

    docs(IFieldDiscoveryService): enhance XML documentation for field discovery methods

--- FirebirdTraceAnalyzer/Interfaces/IFieldDiscoveryService.cs
+++ FirebirdTraceAnalyzer/Interfaces/IFieldDiscoveryService.cs
@@ -4,37 +4,47 @@ using FirebirdTraceParser.Models.Events;
 namespace FirebirdTraceAnalyzer.Interfaces;
 
 /// <summary>
-/// Сервис для обнаружения доступных полей в событиях трассировки
+/// Defines a service for discovering fields from trace events.
 /// </summary>
 public interface IFieldDiscoveryService
 {
     /// <summary>
-    /// Получает все общие поля (пересечение) для указанных событий
+    /// Retrieves all common fields (intersection) for the specified events.
     /// </summary>
+    /// <param name="events">The events for which to retrieve common fields.</param>
+    /// <returns>The list of common fields.</returns>
     IReadOnlyList<DiscoveredField> GetCommonFields(IEnumerable<EventBase> events);
     
     /// <summary>
-    /// Получает все поля для конкретного типа события
+    /// Retrieves all fields for the specified event type.
     /// </summary>
+    /// <param name="eventType">The type of the event for which to retrieve fields.</param>
+    /// <returns>The list of fields.</returns>
     IReadOnlyList<DiscoveredField> GetFieldsForType(Type eventType);
     
     /// <summary>
-    /// Получает только сортируемые поля
+    /// Retrieves all sortable fields for the specified events.
     /// </summary>
+    /// <param name="events">The events for which to retrieve sortable fields.</param>
+    /// <returns>The list of sortable fields.</returns>
     IReadOnlyList<DiscoveredField> GetSortableFields(IEnumerable<EventBase> events);
     
     /// <summary>
-    /// Получает только фильтруемые поля
+    /// Retrieves all filterable fields for the specified events.
     /// </summary>
+    /// <param name="events">The events for which to retrieve filterable fields.</param>
+    /// <returns>The list of filterable fields.</returns>
     IReadOnlyList<DiscoveredField> GetFilterableFields(IEnumerable<EventBase> events);
     
     /// <summary>
-    /// Получает все доступные поля (объединение всех типов)
+    /// Retrieves all available fields for the specified events.
     /// </summary>
+    /// <param name="events">The events for which to retrieve available fields.</param>
+    /// <returns>The list of available fields.</returns>
     IReadOnlyList<DiscoveredField> GetAllAvailableFields(IEnumerable<EventBase> events);
     
     /// <summary>
-    /// Очищает кэш обнаруженных полей
+    /// Clears the cached field discovery results.
     /// </summary>
     void ClearCache();
 }
