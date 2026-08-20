a0f28667dad18767e097182156dce868add16f64
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-20 08:41:50 +0500

    docs(ISortPlugin): update XML documentation for custom sorting interface

--- FirebirdTraceAnalyzer/Interfaces/Plugins/ISortPlugin.cs
+++ FirebirdTraceAnalyzer/Interfaces/Plugins/ISortPlugin.cs
@@ -2,8 +2,13 @@
 
 namespace FirebirdTraceAnalyzer.Interfaces.Plugins;
 
-/// <summary>Интерфейс для плагинов, предоставляющих кастомные сортировки</summary>
+/// <summary>
+/// Represents a plugin that provides custom sorts for the Firebird Trace Analyzer.
+/// </summary>
 public interface ISortPlugin : IAnalyzerPlugin
 {
+    /// <summary>
+    /// Gets the custom sorts provided by the plugin.
+    /// </summary>
     IEnumerable<SortDescriptor> GetSorts();
 }
