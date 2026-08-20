cd616670718b994492cded0622a9588150b7124e
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-20 08:31:47 +0500

    docs(IAnalyzerPlugin): enhance XML documentation for plugin interface properties

--- FirebirdTraceAnalyzer/Interfaces/Plugins/IAnalyzerPlugin.cs
+++ FirebirdTraceAnalyzer/Interfaces/Plugins/IAnalyzerPlugin.cs
@@ -1,10 +1,27 @@
 ﻿namespace FirebirdTraceAnalyzer.Interfaces.Plugins;
 
-/// <summary>Базовый интерфейс для любого плагина системы</summary>
+/// <summary>
+/// Represents a general interface of plugin for the Firebird Trace Analyzer.
+/// </summary>
 public interface IAnalyzerPlugin
 {
+    /// <summary>
+    /// Gets the unique identifier of the plugin.
+    /// </summary>
     string Id { get; }
+    
+    /// <summary>
+    /// Gets the name of the plugin.
+    /// </summary>
     string Name { get; }
+    
+    /// <summary>
+    /// Gets the author of the plugin.
+    /// </summary>
     string Author { get; }
+    
+    /// <summary>
+    /// Gets the version of the plugin.
+    /// </summary>
     string Version { get; }
 }
