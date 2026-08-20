0853856d42614435913d581c111d43c6b848949f
deliciousNesquik <byte.in4matic@gmail.com>
2026-08-20 10:30:18 +0500

    docs(ReportDesignSessionContext): enhance XML documentation for report design session context properties

--- FirebirdTraceAnalyzer/Models/Reports/ReportDesignSessionContext.cs
+++ FirebirdTraceAnalyzer/Models/Reports/ReportDesignSessionContext.cs
@@ -3,13 +3,22 @@ using FirebirdTraceParser.Models.Events;
 namespace FirebirdTraceAnalyzer.Models.Reports;
 
 /// <summary>
-/// Данные текущей сессии анализа для превью и генерации отчётов в дизайнере.
+/// Represents the context of a report design session, including source events, files, and total event count.
 /// </summary>
 public sealed class ReportDesignSessionContext
 {
+    /// <summary>
+    /// Source events for the current session. These events are used to generate reports and previews in the report designer.
+    /// </summary>
     public required IReadOnlyList<EventBase> SourceEvents { get; init; }
 
+    /// <summary>
+    /// Files that were used to generate the current session. These files are used to generate reports and previews in the report designer.
+    /// </summary>
     public required IReadOnlyList<TraceFileInfoModel> Files { get; init; }
 
+    /// <summary>
+    /// Total number of events in the current session. This value is used to generate reports and previews in the report designer.
+    /// </summary>
     public required long TotalEventsCount { get; init; }
 }
